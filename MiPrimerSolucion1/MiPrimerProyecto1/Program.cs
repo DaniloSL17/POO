a0b330fd07da1ae0555a76c6218267802f03bbc8
DaniloSL17 <zepoldanilo2001@gmail.com>
2020-01-30 13:28:02 -0700

    hola

---
+++ MiPrimerSolucion1/MiPrimerProyecto1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyecto1
+{
+    class Program
+    {
+        int a, b, c;
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
