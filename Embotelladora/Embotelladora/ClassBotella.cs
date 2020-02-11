13e92447b1d983e79f00fbf172ad1f3892776df3
DaniloSL17 <zepoldanilo2001@gmail.com>
2020-02-10 18:23:41 -0700

    Embotelladora

---
+++ Embotelladora/Embotelladora/ClassBotella.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embotelladora
+{
+    class ClassBotella
+    {
+        //Atributos de la clase
+       public int nivelLlenado;
+        int mililitrosVerificados;
+
+        //Métodos de la clase
+       public String verificarLlenado()
+        {
+            string mensaje;
+            if (nivelLlenado == 250)
+                mensaje = "Llenado correcto";
+            else if (nivelLlenado < 250)
+            {
+                mililitrosVerificados = 250 - nivelLlenado;
+                mensaje = "Le faltan " + mililitrosVerificados + "mililitros ";
+            }
+            else
+            {
+                mililitrosVerificados = nivelLlenado - 250;
+                mensaje = "Le sobran " + mililitrosVerificados + "mililitros ";
+            }
+            return mensaje;
+
+        }
+
+
+    }
+}
